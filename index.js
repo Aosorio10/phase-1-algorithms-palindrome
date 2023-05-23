b115b3b1a875d37229c779345ab9a299902cd21d
Alejandro Osorio <avot3110@hotmail.com>
2023-05-23 10:55:29 -0400

    lab solutions Palindrome

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 function isPalindrome(word) {
-  word = word.toLowerCase();
   const reversed = word.split('').reverse().join('');
   return word === reversed;
 }
 
 /* 
-  Add your pseudocode here
+  create a function with the palindrome 
+  look up what it meas, base on that create it 
+  make sure that return what we need 
 */
 
 /*
-  Add written explanation of your solution here
+ first, I have to look up what it is, once I knew what it was 
+   it click in my head that I have to create a function that it would read 
+   the words in reverse but it will remain the same. that is why i came up the 
+   slipt() reverse() and join(), basically what split does is split the word in 
+   to an array, then the reverse is to reverse the order and the join is 
+   just to put it back into a single string.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
